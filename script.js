e54a4017e68c1dc0203f4d75fe8b60e123a7c110
Diwas KC <diwaskc12@gmail.com>
2023-04-02 12:58:18 +0545

    signup form complete

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ submitButton.addEventListener("click", function (e) {
   let signupForm = document.forms["signupForm"];
   let firstNameInput = signupForm.firstName;
   let lastNameInput = signupForm.lastName;
+  let passwordInput = signupForm.password;
 
   if (firstNameInput.value == "") {
     errors.firstName = "Firstname Should not be empty";
@@ -15,13 +16,24 @@ submitButton.addEventListener("click", function (e) {
     errors.lastName = "Lastname should not be empty";
   }
 
+  if (passwordInput.value == "") {
+    errors.password = "Password should not be empty";
+  }
+
+  if (passwordInput.value.length <= 5) {
+    errors.password = "Minimum 6 characters are needed";
+  }
+
   console.log(errors);
 
   for (let error in errors) {
+    let lastNameGroup = document.getElementById(`${error}Group`);
+    let lastNameMessage = document.querySelector(`#${error}Group .message`);
+    lastNameGroup.classList.add("error");
+    lastNameMessage.textContent = errors[error];
+  }
+
+  if (Object.keys(errors).length == 0) {
+    signupForm.submit();
   }
 });
-
-// let lastNameGroup = document.getElementById("lastNameGroup");
-// let lastNameMessage = document.querySelector("#lastNameGroup .message");
-// lastNameGroup.classList.add("error");
-// lastNameMessage.textContent = "Lastname should not be empty";
