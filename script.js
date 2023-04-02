c0ebadfa17e8def9931236e926bfa9eec70ee3e8
Diwas KC <diwaskc12@gmail.com>
2023-04-02 12:58:26 +0545

    js file added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let errors = {};
 
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
+  errors = {};
   let signupForm = document.forms["signupForm"];
   let firstNameInput = signupForm.firstName;
   let lastNameInput = signupForm.lastName;
